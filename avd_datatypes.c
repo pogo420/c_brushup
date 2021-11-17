575a59d8448f6dafb85daa58c91fff3c9f27d60c
Arnab Mukherjee <am0072008@gmail.com>
2021-11-17 13:30:35 +0530

    struct and typedefs

--- avd_datatypes.c
+++ avd_datatypes.c
@@ -9,6 +9,13 @@ enum age_type {
 
 };
 
+typedef struct point
+{
+    int x;
+    int y;
+} Point;
+
+
 struct avd_datatypes {
 
     char name[100];
@@ -32,6 +39,7 @@ int main() {
     printf("%s - %lu : %s - %lu \n", string1, sizeof(string1), string2, sizeof(string2));
 
     struct avd_datatypes person;
+    struct avd_datatypes person2 = {0};  // standard way to initalize
 
     strcpy(person.name, "Pogo");
     person.age_static = OLD;
@@ -42,6 +50,12 @@ int main() {
     modify(&person);
     printf("%d\n", person.age_static);
     printf("%s\n", person.name);
-
+    printf("----------------------------------\n");
+    printf("-%d\n", person2.age_static);
+    printf("-%s\n", person2.name);
+    printf("----------------------------------\n");
+    Point p = {0};
+    printf("-%d\n", p.x);
+    printf("-%d\n", p.y);
     return 0;
 }

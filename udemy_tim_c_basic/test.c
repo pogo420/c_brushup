6bc27c9f604ee12c37281024abbb8ccb80086a6b
Arnab Mukherjee <am0072008@gmail.com>
2022-01-09 20:27:18 +0530

    updates 09-01-2022

--- udemy_tim_c_basic/test.c
+++ udemy_tim_c_basic/test.c
@@ -3,6 +3,10 @@
 int main()
 {
     /* code */
-    printf("Hello Gorillaz\n");
+    char inp[10];
+    int i;
+    scanf("%d,%s",&i, inp);
+    // scanf("%s", &inp[0]); // Also work
+    printf("%s-%s-%d\n", "Hello Gorillaz", inp, i);
     return 0;
 }

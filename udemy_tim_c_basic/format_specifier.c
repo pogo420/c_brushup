f8b6bcb040791e9f926f1c2049c40a2fe81d3827
Arnab Mukherjee <am0072008@gmail.com>
2022-01-13 15:41:45 +0530

    format specifier

---
+++ udemy_tim_c_basic/format_specifier.c
@@ -0,0 +1,7 @@
+# include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("the value is: %d, his name is %s, float number: %f, double_number: %g and char: %c\n", 32, "OlaBoo", 34.56f, 89990.909,'a');
+    return 0;
+}

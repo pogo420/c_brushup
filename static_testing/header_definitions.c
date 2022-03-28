430e112fc12b60119a6c78b11abb3aa91317ae73
Arnab Mukherjee <am0072008@gmail.com>
2022-03-28 23:07:49 +0530

    static updates

---
+++ static_testing/header_definitions.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+
+ void check() {
+     for(int i = 0; i < 10; i++)
+    printf("Hello static: %d\n", i);
+}
---
+++ static_testing/static_testing.c
@@ -0,0 +1,10 @@
+// #include "check_header.h"
+// #include "header_definitions.c"
+
+void check();
+
+int main(int argc, char const *argv[])
+{
+    check();
+    return 0;
+}
